3afea97ecb8357aed122d2637f7eb661580b6cfa
Sendhil Panchadsaram <sendhilp@gmail.com [alias]>
2018-12-09 20:05:52 -0800

    Tweaking init script

---
+++ fedora/README.md
@@ -0,0 +1,3 @@
+# Fedora
+
+curl -s https://raw.githubusercontent.com/sendhil/linux-lapto-setup/master/init/fedora.sh | bash
--- fedora/install-software.sh
+++ fedora/install-software.sh
@@ -5,6 +5,26 @@ if [ "$EUID" -ne 0 ]
   exit
 fi
 
-# i3 + i3 gaps
-dnf install i3
-dnf install libxcb-devel xcb-util-keysyms-devel xcb-util-devel xcb-util-wm-devel xcb-util-xrm-devel yajl-devel libXrandr-devel startup-notification-devel libev-devel xcb-util-cursor-devel libXinerama-devel libxkbcommon-devel libxkbcommon-x11-devel pcre-devel pango-devel git gcc automake 
+mkdir -p ~/src
+
+# Dev Tools
+dnf install -y make
+
+# i3
+dnf install -y i3
+
+# i3 gaps
+dnf install -y libxcb-devel xcb-util-keysyms-devel xcb-util-devel xcb-util-wm-devel xcb-util-xrm-devel yajl-devel libXrandr-devel startup-notification-devel libev-devel xcb-util-cursor-devel libXinerama-devel libxkbcommon-devel libxkbcommon-x11-devel pcre-devel pango-devel git gcc automake 
+cd ~/src
+git clone https://github.com/Airblader/i3.git i3-gaps
+cd ~/src/i3-gaps
+autoreconf --force --install
+rm -rf build/
+mkdir -p build && cd build/
+# Disabling sanitizers is important for release versions!
+# The prefix and sysconfdir are, obviously, dependent on the distribution.
+../configure --prefix=/usr --sysconfdir=/etc --disable-sanitizers
+make
+make install
+
+
