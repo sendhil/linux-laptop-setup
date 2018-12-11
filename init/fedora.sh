515e40abab81c420aa0c7bb3e1194e7c455f4c27
Sendhil Panchadsaram <sendhilp@gmail.com [alias]>
2018-12-11 15:09:46 -0800

    Adjusting init script to clone dotfiles as well.

--- init/fedora.sh
+++ init/fedora.sh
@@ -8,8 +8,11 @@ sudo dnf install -yq git
 if [ ! -d ~/src/linux-laptop-setup ]; then
   echo "Cloning Linux Laptop Setup..."
   git clone https://github.com/sendhil/linux-laptop-setup ~/src/linux-laptop-setup
+  git clone https://github.com/sendhil/dotfiles ~/src/dotfiles
 else
   echo "Updating Linux Laptop Setup"
   cd ~/src/linux-laptop-setup
+  git pull --ff-only
+  cd ~/src/dotfiles
   git pull --ff-only
 fi
