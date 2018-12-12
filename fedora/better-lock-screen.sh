b231161d1ec59814ccc091668ff2b80deb394f67
Sendhil Panchadsaram <sendhilp@gmail.com [alias]>
2018-12-11 22:01:25 -0800

    Fixing error with better lock screen setup script

--- fedora/better-lock-screen.sh
+++ fedora/better-lock-screen.sh
@@ -6,4 +6,4 @@ USER_HOME=$(getent passwd $SUDO_USER | cut -d: -f6)
 cd ${USER_HOME}/src
 git clone https://github.com/sendhil/betterlockscreen
 cd betterlockscreen
-cp betterlockscreen ${USER_HOME}/.local/bin
+cp betterlockscreen ${USER_HOME}/.local/bin/
