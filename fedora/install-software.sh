f0c918e8b0f126bba1e7cc13c8905d881b8aa57f
Sendhil Panchadsaram <sendhilp@gmail.com [alias]>
2018-12-11 17:34:44 -0800

    Added a bunch of utilities to install.

--- fedora/install-software.sh
+++ fedora/install-software.sh
@@ -9,14 +9,26 @@ mkdir -p ~/src
 
 # Cish Tools
 dnf install -y make	gcc-c++ clang cmake @development-tools cmake
-dnf install -y neovim vim
 dnf install -y stow
+
+# Dev Env
+dnf install -y neovim
+dnf install -y vim
+
+# Visual Studio Code
+rpm --import https://packages.microsoft.com/keys/microsoft.asc
+sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+dnf check-update
+dnf install code
 
 #Python
 dnf install -y python python3 python-pip
 dnf install -y python-devel
 pip install --user virtualenv
 pip install --user neovim
+
+#GoLang
+dnf install -y golang
 
 #NodeJS
 dnf install -y nodejs
@@ -26,9 +38,40 @@ npm install -g typescript eslint neovim
 dnf install -y ripgrep
 dnf install -y bat
 dnf install -y feh
-
-# Rofi
 dnf install -y rofi
+dnf install -y tmux
+dnf install -y jq
+dnf install -y strace
+dnf install -y dstat
+dnf install -y atop
+dnf install -y htop
+dnf install -y ShellCheck
+dnf install -y gnupg2
+
+# Network
+dnf install -y iperf
+dnf install -y nmap
+dnf install -y ipmitool
+dnf install -y wireshark
+
+# Docker
+dnf install -y docker
+systemctl enable docker
+systemctl start docker
+
+# Disks
+dnf install -y lsscsi
+dnf install -y gparted
+dnf install -y baobab
+
+# libvirt Virtual Machine Manager GUI
+dnf install -y qemu qemu-kvm
+dnf install -y virt-manager
+dnf install -y virt-install
+dnf install -y libvirt
+
+# Gnome 3
+dnf install -y gnome-tweak-tool
 
 # Kitty
 dnf copr enable -y oleastre/kitty-terminal
