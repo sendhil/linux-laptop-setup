fb968b868e3c434da5cec96db270c24da76866fd
Sendhil Panchadsaram <sendhilp@gmail.com>
2018-12-14 01:05:31 -0800

    Tweaking dropbox installer to pull down nautilus-dropbox

--- fedora/dropbox.sh
+++ fedora/dropbox.sh
@@ -2,3 +2,8 @@
 
 cd ~ && wget -O - "https://www.dropbox.com/download?plat=lnx.x86_64" | tar xzf -
 ~/.dropbox-dist/dropboxd
+
+wget "https://www.dropbox.com/download?dl=packages/dropbox.py" -o dropbox
+chmod +x dropbox
+mkdir -p ~/.local/bin
+mv dropbox ~/.local/bin
