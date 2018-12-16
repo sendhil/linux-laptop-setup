8039170b9b29d3f38c6d52063c4e4024bfa0d86c
Sendhil Panchadsaram <sendhilp@gmail.com>
2018-12-16 13:31:47 -0800

    Updating better-lock-screen install to instal i3lock-color

--- fedora/better-lock-screen.sh
+++ fedora/better-lock-screen.sh
@@ -4,6 +4,14 @@
 USER_HOME=$(getent passwd $SUDO_USER | cut -d: -f6)
 
 cd ${USER_HOME}/src
+
+# i3lock-color is required as a dependency
+git clone https://github.com/sendhil/i3lock-color.git
+cd i3lock-color
+sudo dnf install -y cairo-devel libev-devel libjpeg-devel libjpeg-turbo-devel libxcb-devel libxkbcommon-devel libxkbcommon-x11-devel pam-devel pkg-config xcb-util-devel xcb-util-image-devel
+autoreconf -i && ./configure && make
+make install
+
 git clone https://github.com/sendhil/betterlockscreen
 cd betterlockscreen
 mkdir -p ${USER_HOME}/.local/bin
