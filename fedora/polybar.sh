f45abaf484b5b69848bba8f0a7d0503d51338c5d
Sendhil Panchadsaram <sendhilp@gmail.com>
2018-12-16 13:32:20 -0800

    Adding support for wireless tools to the polybar install

--- fedora/polybar.sh
+++ fedora/polybar.sh
@@ -5,7 +5,7 @@
 USER_HOME=$(getent passwd $SUDO_USER | cut -d: -f6)
 
 # Polybar
-dnf install -y cairo-devel libxcb xcb-util-devel libxcb-devel python2 xcb-proto xcb-util-image xcb-util-image-devel xcb-util-wm-devel xcb-util-wm xcb-util-cursor xcb-util-cursor-devel alsa-lib-devel pulseaudio-libs-devel i3-ipc jsoncpp-devel libmpdclient-devel libcurl-devel
+dnf install -y cairo-devel libxcb xcb-util-devel libxcb-devel python2 xcb-proto xcb-util-image xcb-util-image-devel xcb-util-wm-devel xcb-util-wm xcb-util-cursor xcb-util-cursor-devel alsa-lib-devel pulseaudio-libs-devel i3-ipc jsoncpp-devel libmpdclient-devel libcurl-devel wireless-tools-devel
 cd ${USER_HOME}/src
 git clone --recursive https://github.com/jaagr/polybar
 mkdir polybar/build
