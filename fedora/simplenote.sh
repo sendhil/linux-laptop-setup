ffaa3bf3f3c9bb2e687dd14de37e685c9cdc1744
Sendhil Panchadsaram <sendhilp@gmail.com>
2020-05-17 14:45:11 -0700

    Tweaking Simplenote install script.

--- fedora/simplenote.sh
+++ fedora/simplenote.sh
@@ -1,13 +1,12 @@
 #!/bin/bash -e
 
-if ls ~/.local/Simplenote*; then
+if command -v simplenote; then
   echo "Simplenote already installed"
 else
   echo "Installing Simplenote"
   cd ~/
-  wget https://github.com/Automattic/simplenote-electron/releases/download/v1.3.3/Simplenote-linux-1.3.3-x86_64.AppImage
-  chmod +x Simplenote-linux-1.3.3-x86_64.AppImage
-  mv Simplenote-linux-1.3.3-x86_64.AppImage ~/.local
-  ~/.local/Simplenote-linux-1.3.3-x86_64.AppImage &
+  wget https://github.com/Automattic/simplenote-electron/releases/download/v1.16.0/Simplenote-linux-1.16.0-x86_64.rpm
+  rpm -i Simplenote-linux-1.16.0-x86_64.rpm
+  rm Simplenote-linux-1.16.0-x86_64.rpm
 fi
 
