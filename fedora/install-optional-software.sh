5c7eacfee5a3af3f54e766fbd86d8593fb5cf4b9
Sendhil Panchadsaram <sendhilp@gmail.com>
2020-10-19 13:41:32 -0700

    Few tweaks

--- fedora/install-optional-software.sh
+++ fedora/install-optional-software.sh
@@ -24,7 +24,6 @@ sudo dnf install -y ncmpcpp
 sudo dnf install -y redshift
 sudo dnf install -y pandoc
 sudo dnf install -y peek
-# sudo dnf install -y mongodb
 
 # Slack
 sudo dnf install -y flatpak
@@ -39,24 +38,7 @@ dnf copr enable evana/fira-code-fonts
 dnf install fira-code-fonts
 
 # Visual Studio Code
-if [ -f /etc/redhat-release ]; then
-  rpm --import https://packages.microsoft.com/keys/microsoft.asc
-  sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
-  dnf check-update
-  dnf install -y code
-else
-  curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
-  install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
-  sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
-  apt-get install apt-transport-https
-  apt-get update
-  apt-get install code # or code-insiders
-fi
-
-
-if [ -f /etc/redhat-release ]; then
-  dnf install -y redshift
-else
-  apt-get install redshift
-fi
-
+rpm --import https://packages.microsoft.com/keys/microsoft.asc
+sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+dnf check-update
+dnf install -y code
